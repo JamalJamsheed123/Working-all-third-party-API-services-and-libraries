aaa1366710a2d55e7030fd6d3d96cf47e34aeb6a
JamalJamsheed123~ <jamal.jamsheed2@gmail.com>
2024-12-26 01:28:11 +0500

    Update Code
    Update README.md File

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Working-all-third-party-API-services-and-libraries
+
+## I will try to use all new technologies: Glide, Picasso, Jetpack compose, HttpURLConnection, OkHttp, Json, Gson, RxJava, coroutines, Flow, Retrofit, MVP and others. Exactly the ones that are required in the android development for any App Thanks.
---
+++ app/src/main/java/com/example/demoapp_working_all_third_party_api_services/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.demoapp_working_all_third_party_api_services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
