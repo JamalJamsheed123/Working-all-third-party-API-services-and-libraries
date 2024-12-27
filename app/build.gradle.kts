2bfe3e03f22dad63399e8ed0a46cf7dc31e0d763
JamalJamsheed123~ <jamal.jamsheed2@gmail.com>
2024-12-27 19:00:11 +0500

    Image Loader APP
    - Display the image using Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // image loading Dependencies
+    implementation("com.github.bumptech.glide:glide:4.13.2")
+    implementation("com.github.bumptech.glide:compiler:4.11.0")
 }
--- app/src/main/java/com/example/demoapp_working_all_third_party_api_services/MainActivity.kt
+++ app/src/main/java/com/example/demoapp_working_all_third_party_api_services/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.demoapp_working_all_third_party_api_services
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide
+            .with(this)
+            .load("https://avatars.githubusercontent.com/u/1342004?v=4")
+            .into(findViewById(R.id.imageView))
     }
 }
